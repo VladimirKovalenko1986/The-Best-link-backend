6a3a4134131326cc7170c6672e1839675d391bac
Volodymyr Kovalenko <dante77758@gmail.com>
2024-09-23 22:41:29 +0300

    add autherization bearer token

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,50 @@
+import createHttpError from 'http-errors';
+import { User } from '../db/models/User.js';
+import { Sessions } from '../db/models/Session.js';
+
+const authenticate = async (req, res, next) => {
+  const authHeader = req.get('Authorization');
+
+  if (!authHeader) {
+    next(createHttpError(401, 'Palease provide Authorization header'));
+    return;
+  }
+
+  const bearer = authHeader.split(' ')[0];
+  const token = authHeader.split(' ')[1];
+
+  if (bearer !== 'Bearer' || !token) {
+    next(createHttpError(401, 'Auth header should be of type Bearer'));
+    return;
+  }
+
+  const session = await Sessions.findOne({
+    accessToken: token,
+  });
+
+  if (!session) {
+    next(createHttpError(401, 'Session not found'));
+    return;
+  }
+
+  const isAccessTokenExpired =
+    new Date() > new Date(session.accessTokenValidUntil);
+
+  if (isAccessTokenExpired) {
+    next(createHttpError(401, 'Access token expired'));
+    return;
+  }
+
+  const user = await User.findById(session.userId);
+
+  if (!user) {
+    next(createHttpError(401));
+    return;
+  }
+
+  req.user = user;
+
+  next();
+};
+
+export default authenticate;
--- src/routers/links-router.js
+++ src/routers/links-router.js
@@ -11,8 +11,11 @@ import validateBody from '../middlewares/validateBody.js';
 import { createLinkSchema, updateLinkSchema } from '../validation/links.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import isValidId from '../middlewares/isValidId.js';
+import authenticate from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getLinksController));
 
