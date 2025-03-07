8ca5bd147388e1bc1d7f794f4977e46ae874e0f9
Volodymyr Kovalenko <dante77758@gmail.com>
2025-03-07 17:31:05 +0200

    add redirect

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,21 +1,23 @@
 import { OAuth2Client } from 'google-auth-library';
-import path from 'node:path';
-import { readFile } from 'fs/promises';
+// import path from 'node:path';
+// import { readFile } from 'fs/promises';
 import env from './env.js';
 import createHttpError from 'http-errors';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+// const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+// const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: env('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: env('GOOGLE_AUTH_REDIRECT_URI'),
+  // redirectUri: oauthConfig.web.redirect_uris[0],
 });
 
 const generateAuthUrl = () =>
   googleOAuthClient.generateAuthUrl({
+    redirect_uri: env('GOOGLE_AUTH_REDIRECT_URI'),
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
