9be611a875b24a55bb3a2f8ac8e24885aa6e5182
Volodymyr Kovalenko <dante77758@gmail.com>
2025-06-21 20:21:44 +0300

    add prodaction TS

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const startServer = () => {
         'http://localhost:5173',
         'https://the-best-link-frontend.onrender.com',
         'https://the-best-link-frontend.vercel.app',
-        'https://the-best-link-frontend-ts-mn55.vercel.app/login',
+        'https://the-best-link-frontend-ts-mn55.vercel.app',
       ], // ✅ Дозволяє фронт
       credentials: true, // ✅ Дозволяє передавати куки
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Дозволені методи
